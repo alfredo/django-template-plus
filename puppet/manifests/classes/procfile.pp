f31695dee7c867e556cf15e81885d4b5bba7401d
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-07-04 12:38:12 +0100

    Simplify the script for procfile.

--- puppet/manifests/classes/procfile.pp
+++ puppet/manifests/classes/procfile.pp
@@ -11,24 +11,19 @@ class procfile ($project_path, $project_name) {
     provider => 'gem',
   }
 
-  exec { 'force-upstart':
-    cwd => "/etc/init",
-    command => "rm -rf /etc/init/$project_name*",
-    require => Package[$packages],
-  }
-
   exec { 'install-upstart':
     cwd => "$project_path",
     command => "foreman export -a $project_name -p 8000 upstart /etc/init -u vagrant -f $project_path/Procfile.local",
-    require => Exec['force-upstart'],
-    path => ["/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/opt/vagrant_ruby/bin"],
+    require => Package[$packages],
+    creates => ['/etc/init/$project_name.conf'],
+    environment => "HOME=/home/vagrant",
   }
 
   service { $project_name:
     ensure => running,
     enable => true,
     provider => upstart,
-    subscribe => [Package['foreman'], Exec['install-upstart']],
+    subscribe => Exec['install-upstart'],
   }
 
 }
