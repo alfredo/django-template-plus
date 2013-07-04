0057502da645c4bc86f467d8a2f1eb27b8eff75e
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-07-04 12:34:04 +0100

    Changed deprecated simple view to use the CVB replacement.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', direct_to_template, {'template': 'base.html'}),
+    url(r'^$', TemplateView.as_view(template_name="base.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
